a86222dc9d57ab22388171d109e8fc053696b790
kandaliakiki <kandaliak1k1@gmail.com>
2022-12-15 00:17:08 +0700

    initalize spring boot and react js

---
+++ waproject/src/main/java/com/springkiki/waproject/WaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.springkiki.waproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaprojectApplication.class, args);
+	}
+
+}
---
+++ waproject/src/test/java/com/springkiki/waproject/WaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springkiki.waproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
